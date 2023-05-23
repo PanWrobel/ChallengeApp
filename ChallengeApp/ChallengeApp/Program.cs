063cd2e964a86aa8acbb123c90cff936cb80c493
Krzysztof <Krzysztof.Kacik@gmail.com>
2023-05-23 08:57:43 +0200

    Zadanie nr 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,19 @@
+﻿string name = "Ewa";
+string gender = "female";
+int age = 33;
+
+if (gender == "female")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30-tego roku życia");
+    }
+    else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa , lat 33");
+    }
+}
+else if (age < 18) 
+{
+    Console.WriteLine("Męszczyzna , niepełnoletni");
+}
