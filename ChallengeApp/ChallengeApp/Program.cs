7991691ffc7e662e658901e21ec90b609df228ed
Krzysztof <Krzysztof.Kacik@gmail.com>
2023-10-01 21:31:13 +0200

    Zadanie domowe nr 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Numerics;
+
+string name = "Magda";
+string gender = "Female";
+var age = 30;
+if (age < 30)
+    if (gender == "Female")
+    {
+        Console.WriteLine("kobieta, poniżej 33 lat.");
+    }
+    else
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna.");
+    }
+else if (name == "kobieta" && age == 33)
+{
+    Console.WriteLine("To jest Eawa i ma 33 lata ");
+}
+else if (gender != "Female" && age < 18)
+{
+    Console.WriteLine("Niepełnoletnia Kiebieta");
+}
